beaa9ab20adf13b5c45a035e7eff769a45bad659
Madaraka02 <victormadaraka@gmail.com>
2022-07-20 12:04:27 +0300

    added client model

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'customers',
     'rest_framework',
     'crispy_forms',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'crm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from autoslug import AutoSlugField
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 
 # Create your models here.
+#  slug = AutoSlugField(populate_from='title')
+class Client(models.Model):
+    username = models.CharField(max_length=300, null=True, blank=True,unique=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=300, null=True, blank=True)
+    slug = AutoSlugField(populate_from='username')
+    age = models.IntegerField(default=1, null=True, blank=True)
+    description = models.TextField(null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
